6a8492c48d86dcf4cba8a8ce081b66251c1fea1c
Stefan Bellon <1427724+sbellon@users.noreply.github.com>
2024-10-10 14:00:17 +0200

    remove requirement to configure service group name, just read in all event service names and make them available

--- ChurchSong.py
+++ ChurchSong.py
@@ -11,6 +11,7 @@ import subprocess
 import sys
 import typing
 import zipfile
+from collections import defaultdict
 
 import pptx
 import requests
@@ -34,10 +35,6 @@ class Configuration:
     @property
     def login_token(self) -> str:
         return self._expand_vars('ChurchTools.Settings', 'login_token')
-
-    @property
-    def kigo_group(self) -> str:
-        return self._expand_vars('ChurchTools.Settings', 'kigo_group')
 
     @property
     def person_dict(self) -> dict[str, str]:
@@ -66,14 +63,13 @@ class Configuration:
         ]
 
 
-JSON = dict[str, typing.Any]
+JSON = typing.Any
 
 
 class ChurchTools:
     def __init__(self, config: Configuration) -> None:
         self._base_url = config.base_url
         self._login_token = config.login_token
-        self._kigo_group = config.kigo_group
         self._person_dict = config.person_dict
         self._temp_dir = config.temp_dir
 
@@ -99,13 +95,17 @@ class ChurchTools:
         r.raise_for_status()
         return r
 
-    def _get(self, url: str, params: dict[str, str] | None = None) -> requests.Response:
+    def _get(
+        self,
+        url: str,
+        params: dict[str, str] | None = None,
+    ) -> requests.Response:
         return self._request('GET', url, params)
 
     def _post(
         self,
         url: str,
-        params: JSON | None = None,
+        params: dict[str, str] | None = None,
     ) -> requests.Response:
         return self._request('POST', url, params)
 
@@ -121,6 +121,9 @@ class ChurchTools:
         r = self._get('/api/events', params={'from': from_date} if from_date else None)
         return r.json()['data']
 
+    def _get_next_event(self, from_date: str | None = None) -> JSON:
+        return self._get_events(from_date)[0]
+
     def _get_event(self, event_id: int) -> JSON:
         r = self._get(f'/api/events/{event_id}')
         return r.json()['data']
@@ -134,50 +137,35 @@ class ChurchTools:
             f'/api/agendas/{agenda_id}/export',
             params={
                 'target': 'SONG_BEAMER',
-                'exportSongs': True,
-                'appendArrangement': False,
-                'withCategory': False,
+                'exportSongs': 'true',
+                'appendArrangement': 'false',
+                'withCategory': 'false',
             },
         )
         return r.json()['data']
 
-    def get_kigo_team(self, from_date: str | None = None) -> dict[str, str]:
-        kigo_group_id = None
-        for servicegroup in self._get_servicegroups():
-            if servicegroup.get('name') == self._kigo_group:
-                kigo_group_id = servicegroup['id']
-        if kigo_group_id is None:
-            sys.stderr.write(f"Cannot find service group '{self._kigo_group}'.\n")
-            return {}
-        kigo_groups = {
-            service['id']: service['name']
-            for service in self._get_services()
-            if service.get('serviceGroupId') == kigo_group_id
-        }
-        next_event = self._get_events(from_date)[0]
-        _date = next_event['startDate'][0:10]
+    def get_service_leads(self, from_date: str | None = None) -> defaultdict[str, str]:
+        services = {service['id']: service['name'] for service in self._get_services()}
+        next_event = self._get_next_event(from_date)
         event = self._get_event(next_event['id'])
-        # Initialize result with all possible groups and the "None" person.
-        kigo_team = {
-            kigo_group: self._person_dict.get(str(None), str(None))
-            for kigo_group in kigo_groups.values()
-        }
+        # Initialize the "None" person for all services.
+        service_leads = defaultdict(lambda: self._person_dict.get(str(None), str(None)))
         # Update with the actual persons of the eventservice.
-        kigo_team.update(
+        service_leads.update(
             {
-                kigo_group: self._person_dict.get(
+                service_name: self._person_dict.get(
                     eventservice['name'],
                     eventservice['name'],
                 )
                 for eventservice in event['eventServices']
-                for kigo_id, kigo_group in kigo_groups.items()
-                if eventservice['serviceId'] == kigo_id
+                for service_id, service_name in services.items()
+                if eventservice['serviceId'] == service_id
             },
         )
-        return kigo_team
+        return service_leads
 
     def get_url_for_songbeamer_agenda(self, from_date: str | None = None) -> str:
-        next_event = self._get_events(from_date)[0]
+        next_event = self._get_next_event(from_date)
         date = next_event['startDate'][0:10]
         try:
             agenda = self._get_event_agenda(next_event['id'])
@@ -201,11 +189,11 @@ class PowerPoint:
         self._template_pptx = config.template_pptx
         self._prs = pptx.Presentation(os.fspath(self._template_pptx))
 
-    def create(self, kigo_team: dict[str, str]) -> str:
+    def create(self, service_leads: dict[str, str]) -> None:
         slide_layout = self._prs.slide_layouts[0]
         slide = self._prs.slides.add_slide(slide_layout)
         for ph in slide.placeholders:
-            name = kigo_team.get(ph._base_placeholder.name)  # noqa: SLF001
+            name = service_leads[ph._base_placeholder.name]  # noqa: SLF001
             if isinstance(ph, pptx.shapes.placeholder.PicturePlaceholder):
                 ph.insert_picture(os.fspath(self._portraits_dir / f'{name}.jpeg'))
             elif (
@@ -259,10 +247,10 @@ def main() -> None:
 
     try:
         ct = ChurchTools(config)
-        kigo_team = ct.get_kigo_team(args.from_date)
+        service_leads = ct.get_service_leads(args.from_date)
 
         pp = PowerPoint(config)
-        pp.create(kigo_team)
+        pp.create(service_leads)
         pp.save()
 
         ct.download_and_extract_agenda_zip(
