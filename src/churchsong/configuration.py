0e6b371e244675e34f356ae3cb463e3a4af8f4c6
Stefan Bellon <1427724+sbellon@users.noreply.github.com>
2025-04-16 18:14:37 +0200

    added SPDX license identifier

--- src/churchsong/configuration.py
+++ src/churchsong/configuration.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2024-2025 Stefan Bellon
+#
+# SPDX-License-Identifier: MIT
+
 from __future__ import annotations
 
 import gettext
--- typings/__builtins__.pyi
+++ typings/__builtins__.pyi
@@ -1,2 +1,6 @@
+# SPDX-FileCopyrightText: 2024-2025 Stefan Bellon
+#
+# SPDX-License-Identifier: MIT
+
 # __builtins__.pyi
 def _(s: str) -> str: ...
